463c11b08ee6fee0602d9830c92fda849e2b7811
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-07-27 08:18:29 -0300

    Setup Linters

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	tabWidth: 3,
+	useTabs: true,
+	semi: false,
+	arrowParens: "avoid",
+	//printWidth: 90,
+	trailingComma: "all",
+	proseWrap: "preserve",
+	singleQuote: false,
+	jsxBracketSameLine: false,
+	bracketSpacing: true,
+}
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+	return (
+		<View>
+			{/* Video Component */}
+			<View>
+				{/* Thumbnail */}
+				<Image
+					style={styles.thumbnail}
+					source={{
+						uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/thumbnails/thumbnail1.jpeg",
+					}}
+				/>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	thumbnail: { width: 250, height: 250 },
+});
