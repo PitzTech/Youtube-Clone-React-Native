6ae6b771f218734b902583b334eed2e1938e9ef4
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-08-14 14:00:27 -0300

    Update Screen Video Implementation

--- screens/VideoScreen/VideoScreen.tsx
+++ screens/VideoScreen/VideoScreen.tsx
@@ -31,140 +31,165 @@ export default function VideoScreen(): JSX.Element {
 				paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
 			}}
 		>
-			<ScrollView showsVerticalScrollIndicator={false}>
-				{/* Video Player */}
-				<Image
-					style={styles.videoPlayer}
-					source={{ uri: video.thumbnail }}
-				/>
+			{/* Video Player */}
+			<Image style={styles.videoPlayer} source={{ uri: video.thumbnail }} />
 
-				{/* Video Info */}
-				<View style={styles.videoInfoContainer}>
-					<Text style={styles.tags}>{video.tags}</Text>
-					<Text style={styles.title}>{video.title}</Text>
-					<Text style={styles.subtitle}>
-						{video.user.name} • {viewString} visualizações • há{" "}
-						{video.createdAt}
-					</Text>
-				</View>
+			{/* Recommended Videos with video info on header */}
 
-				{/* Action List */}
-				<View style={styles.actionListContainer}>
-					<ScrollView horizontal showsHorizontalScrollIndicator={false}>
-						<View style={styles.actionListItem}>
-							<AntDesign name="like1" size={25} color="lightgrey" />
-							<Text style={styles.actionText}>{video.likes}</Text>
+			<FlatList
+				data={videos}
+				renderItem={({ item }) => <VideoListItem video={item} />}
+				ListHeaderComponent={() => (
+					<>
+						{/* Video Info */}
+						<View style={styles.videoInfoContainer}>
+							<Text style={styles.tags}>{video.tags}</Text>
+							<Text style={styles.title}>{video.title}</Text>
+							<Text style={styles.subtitle}>
+								{video.user.name} • {viewString} visualizações • há{" "}
+								{video.createdAt}
+							</Text>
 						</View>
-						<View style={styles.actionListItem}>
-							<AntDesign name="dislike2" size={25} color="lightgrey" />
-							<Text style={styles.actionText}>{video.dislikes}</Text>
-						</View>
-						<View style={styles.actionListItem}>
-							<AntDesign name="export" size={25} color="lightgrey" />
-							<Text style={styles.actionText}>{video.dislikes}</Text>
-						</View>
-						<View style={styles.actionListItem}>
-							<AntDesign name="download" size={25} color="lightgrey" />
-							<Text style={styles.actionText}>{video.dislikes}</Text>
-						</View>
-					</ScrollView>
-				</View>
 
-				{/* User Info */}
-				<View
-					style={{
-						flexDirection: "row",
-						alignItems: "center",
-						borderColor: "#3d3d3d",
-						borderTopWidth: 0.5,
-						borderBottomWidth: 0.5,
-						padding: 15,
-					}}
-				>
-					<Image
-						style={styles.avatar}
-						source={{ uri: video.user.image }}
-					/>
+						{/* Action List */}
+						<View style={styles.actionListContainer}>
+							<ScrollView
+								horizontal
+								showsHorizontalScrollIndicator={false}
+							>
+								<View style={styles.actionListItem}>
+									<AntDesign
+										name="like1"
+										size={25}
+										color="lightgrey"
+									/>
+									<Text style={styles.actionText}>{video.likes}</Text>
+								</View>
+								<View style={styles.actionListItem}>
+									<AntDesign
+										name="dislike2"
+										size={25}
+										color="lightgrey"
+									/>
+									<Text style={styles.actionText}>
+										{video.dislikes}
+									</Text>
+								</View>
+								<View style={styles.actionListItem}>
+									<AntDesign
+										name="export"
+										size={25}
+										color="lightgrey"
+									/>
+									<Text style={styles.actionText}>
+										{video.dislikes}
+									</Text>
+								</View>
+								<View style={styles.actionListItem}>
+									<AntDesign
+										name="download"
+										size={25}
+										color="lightgrey"
+									/>
+									<Text style={styles.actionText}>
+										{video.dislikes}
+									</Text>
+								</View>
+							</ScrollView>
+						</View>
 
-					<View style={{ marginHorizontal: 10, flex: 1 }}>
-						<Text
+						{/* User Info */}
+						<View
 							style={{
-								fontSize: 18,
-								color: "white",
-								fontWeight: "bold",
+								flexDirection: "row",
+								alignItems: "center",
+								borderColor: "#3d3d3d",
+								borderTopWidth: 0.5,
+								borderBottomWidth: 0.5,
+								padding: 15,
 							}}
 						>
-							{video.user.name}
-						</Text>
-						<Text style={{ fontSize: 15, color: "grey" }}>
-							{video.user.subscribers} inscritos
-						</Text>
-					</View>
+							<Image
+								style={styles.avatar}
+								source={{ uri: video.user.image }}
+							/>
 
-					<View
-						style={{
-							paddingHorizontal: 10,
-							alignItems: "center",
-							justifyContent: "center",
-						}}
-					>
-						<Text
-							style={{
-								fontSize: 18,
-								color: "red",
-								fontWeight: "bold",
-							}}
-						>
-							Inscrever
-						</Text>
-					</View>
-				</View>
+							<View style={{ marginHorizontal: 10, flex: 1 }}>
+								<Text
+									style={{
+										fontSize: 18,
+										color: "white",
+										fontWeight: "bold",
+									}}
+								>
+									{video.user.name}
+								</Text>
+								<Text style={{ fontSize: 15, color: "grey" }}>
+									{video.user.subscribers} inscritos
+								</Text>
+							</View>
 
-				{/* Comments */}
-				<View style={{ padding: 10, marginVertical: 10 }}>
-					<Text
-						style={{
-							color: "lightgrey",
-							fontSize: 16,
-						}}
-					>
-						Comentários{" "}
-						<Text style={{ color: "grey", marginLeft: 10 }}>42</Text>
-					</Text>
+							<View
+								style={{
+									paddingHorizontal: 10,
+									alignItems: "center",
+									justifyContent: "center",
+								}}
+							>
+								<Text
+									style={{
+										fontSize: 18,
+										color: "red",
+										fontWeight: "bold",
+									}}
+								>
+									Inscrever
+								</Text>
+							</View>
+						</View>
 
-					{/* Comment Component */}
-					<View
-						style={{
-							flexDirection: "row",
-							alignItems: "center",
-							marginVertical: 15,
-						}}
-					>
-						<Image
-							style={{ width: 35, height: 35, borderRadius: 18 }}
-							source={{ uri: video.user.image }}
-						/>
+						{/* Comments */}
+						<View style={{ padding: 10, marginVertical: 10 }}>
+							<Text
+								style={{
+									color: "lightgrey",
+									fontSize: 16,
+								}}
+							>
+								Comentários{" "}
+								<Text style={{ color: "grey", marginLeft: 10 }}>
+									42
+								</Text>
+							</Text>
 
-						<Text
-							style={{
-								fontSize: 13,
-								marginLeft: 10,
-								color: "lightgrey",
-							}}
-						>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Quis repudiandae
-						</Text>
-					</View>
-				</View>
+							{/* Comment Component */}
+							<View
+								style={{
+									flexDirection: "row",
+									alignItems: "center",
+									marginVertical: 15,
+								}}
+							>
+								<Image
+									style={{ width: 35, height: 35, borderRadius: 18 }}
+									source={{ uri: video.user.image }}
+								/>
 
-				{/* Recommended Videos */}
-
-				<FlatList
-					data={videos}
-					renderItem={({ item }) => <VideoListItem video={item} />}
-				/>
-			</ScrollView>
+								<Text
+									style={{
+										fontSize: 13,
+										marginLeft: 10,
+										color: "lightgrey",
+									}}
+								>
+									Lorem ipsum dolor sit amet consectetur adipisicing
+									elit. Quis repudiandae
+								</Text>
+							</View>
+						</View>
+					</>
+				)}
+			/>
 		</SafeAreaView>
 	)
 }
